4ae04a93a43e8c220c4ebf35db97281088ec4885
successdevop <successraphael28@gmail.com>
2024-02-11 13:20:15 +0100

    implemented the dashboard controler and authentification functionalities to access the dashboard

--- controllers/main.js
+++ controllers/main.js
@@ -16,6 +16,27 @@ const login = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: `Welcome ${username}`, token });
 };
 
-const dashboard = async (req, res) => {};
+const dashboard = async (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new Unauthenticated("No token provided...");
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const luckyNumber = Math.floor(Math.random() * 100);
+    const decode = jwt.verify(token, process.env.JWT_TKN);
+
+    res.status(StatusCodes.OK).json({
+      msg: `Hello ${decode.username}`,
+      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    });
+  } catch (error) {
+    res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ msg: `Not authorized to access the route...` });
+  }
+};
 
 module.exports = { login, dashboard };
